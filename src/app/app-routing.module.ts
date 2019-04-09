24ec19e2741fdf1cd70d415d71070892611b5add
Jonathan Soh <e0177194@u.nus.edu>
2019-04-09 19:43:34 +0800

    Generate Login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
-  { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule' }
+  { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule' },  { path: 'login', loadChildren: './login/login.module#LoginPageModule' }
+
 ];
 @NgModule({
   imports: [
